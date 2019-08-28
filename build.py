7060648f9e1980f944ba76e9d4b9879ca1d6dd34
Frank T. Bergmann <fbergman@caltech.edu>
2019-08-28 13:19:04 +0200

    - ensure dependencies are uploaded

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True)
+    builder = ConanMultiPackager(upload_dependencies="all")
+    builder.add_common_builds(pure_c=False)
     builder.run()
